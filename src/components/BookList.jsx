9e6b483701e6877f240103d3d4b982345b7a3aa8
bennovandorst <9024828@student.zadkine.nl>
2024-09-10 23:14:39 +0200

    [🤠] Opdracht 1.1

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import Book from "./Book";
 
 const BookList = () => {
-    return (  
-        <>
-        <Book image="https://m.media-amazon.com/images/I/718SstStV3L._AC_UF1000,1000_QL80_.jpg" title="Interesting Facts" author="Jordan Moore"/>
-        <Book image="https://m.media-amazon.com/images/I/81YkqyaFVEL._AC_UF1000,1000_QL80_.jpg" title="Atomic Habits" author="James Clear"/>
-        <Book image="https://media.s-bol.com/Wpzr8Rz7LBEE/mO13Dy3/540x840.jpg" title="Fairy Tale" author="Stephen King"/>
-        </>
-    );
-}
- 
+  const [books] = useState([
+    {
+      image: "https://m.media-amazon.com/images/I/718SstStV3L._AC_UF1000,1000_QL80_.jpg",
+      title: "Interesting Facts",
+      author: "Jordan Moore",
+    },
+    {
+      image: "https://m.media-amazon.com/images/I/81YkqyaFVEL._AC_UF1000,1000_QL80_.jpg",
+      title: "Atomic Habits",
+      author: "James Clear",
+    },
+    {
+      image: "https://media.s-bol.com/Wpzr8Rz7LBEE/mO13Dy3/540x840.jpg",
+      title: "Fairy Tale",
+      author: "Stephen King",
+    },
+  ]);
+
+  return (
+    <>
+      {books.map((book) => (
+        <Book
+          image={book.image}
+          title={book.title}
+          author={book.author}
+        />
+      ))}
+    </>
+  );
+};
+
 export default BookList;
