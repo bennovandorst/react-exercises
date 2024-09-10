8efc5b6d5bf9efb93cdf563bd004d1a9982899ca
bennovandorst <9024828@student.zadkine.nl>
2024-09-10 23:43:38 +0200

    [😡] Ik haat CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import BookList from "./components/BookList"
+import Header from "./components/Header"
+import "./App.css";
 
 function App() {
 
   return (
     <>
+      <Header />
       <BookList />
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return ( 
+        <>
+        <div className="navbar">
+            <a href="#">Home</a>
+            <a href="#">Contact</a>
+            <a href="#">About Us</a>
+        </div>
+        </>
+     );
+}
+ 
+export default Header;
