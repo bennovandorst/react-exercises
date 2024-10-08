4e0310b0f432a19e15584dc116b91ead0b649640
bennovandorst <9024828@student.zadkine.nl>
2024-10-08 11:17:40 +0200

    Gebruik nou die terminal!

---
+++ data.js
@@ -0,0 +1,22 @@
+const books = [
+    {
+        id: 1,
+        image: "https://m.media-amazon.com/images/I/718SstStV3L._AC_UF1000,1000_QL80_.jpg",
+        title: "Interesting Facts",
+        author: "Jordan Moore",
+      },
+      {
+        id: 2,
+        image: "https://m.media-amazon.com/images/I/81YkqyaFVEL._AC_UF1000,1000_QL80_.jpg",
+        title: "Atomic Habits",
+        author: "James Clear",
+      },
+      {
+        id: 3,
+        image: "https://media.s-bol.com/Wpzr8Rz7LBEE/mO13Dy3/540x840.jpg",
+        title: "Fairy Tale",
+        author: "Stephen King",
+      },
+]
+
+export default books;
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,29 +1,34 @@
 import { useState } from "react";
 import Book from "./Book";
+import data from '../../data'
 
 const BookList = () => {
-  const [books] = useState([
-    {
-      image: "https://m.media-amazon.com/images/I/718SstStV3L._AC_UF1000,1000_QL80_.jpg",
-      title: "Interesting Facts",
-      author: "Jordan Moore",
-    },
-    {
-      image: "https://m.media-amazon.com/images/I/81YkqyaFVEL._AC_UF1000,1000_QL80_.jpg",
-      title: "Atomic Habits",
-      author: "James Clear",
-    },
-    {
-      image: "https://media.s-bol.com/Wpzr8Rz7LBEE/mO13Dy3/540x840.jpg",
-      title: "Fairy Tale",
-      author: "Stephen King",
-    },
-  ]);
+  const [books, setBooks] = useState(data);
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  const filteredBooks = data.filter((book) => 
+    book.title.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
     <>
-      {books.map((book) => (
+    <div className='search'>
+      <input 
+      type="text"
+      placeholder="Zoek een titel op"
+      onChange={handleChange}
+      value={searchInput}
+      name="search"
+       />
+    </div>
+      {filteredBooks.map((book) => (
         <Book
+          key={book.id}
           image={book.image}
           title={book.title}
           author={book.author}
