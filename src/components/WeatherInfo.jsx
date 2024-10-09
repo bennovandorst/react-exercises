6a53af9ab41e6a074260869507dcba582548dec9
bennovandorst <9024828@student.zadkine.nl>
2024-10-09 11:11:00 +0200

    voeg icon toe

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -3,7 +3,7 @@ const WeatherInfo = ({weatherData}) => {
       <>
         {weatherData && (
           <div className='weather'>
-            <h2>{weatherData.name} ❄️</h2>
+            <h2>{weatherData.name} <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="weather-icon" /></h2>
             <p>Temperatuur: {weatherData.main.temp} °C</p>
             <p>Gevoelstemperatuur: {weatherData.main.feels_like} °C</p>
             <p>Luchtvochtigheid: {weatherData.main.humidity}%</p>
